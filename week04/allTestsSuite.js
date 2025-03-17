f571d3f022386aed4bfb9861dd554c21ecde05f2
Dierk Koenig <dierk.koenig@canoo.com>
2025-03-17 21:01:39 +0100

    before week 5

--- week04/allTestsSuite.js
+++ week04/allTestsSuite.js
@@ -12,4 +12,3 @@ total.onChange( value => document.getElementById('grossTotal').textContent = "" 
 
 document.querySelector("footer").textContent = "Built with Kolibri " + versionInfo;
 
-document.writeln(12);
